d85e05ef539fa7a003bc9b26d191383feab441e5
arischaefer-dev <arischaefer@gmail.com>
2026-04-17 19:43:09 +0900

    Add PWA manifest, service worker, and icon

---
+++ rsvp/sw.js
@@ -0,0 +1,28 @@
+'use strict';
+const CACHE = 'rsvp-v2';
+const SHELL = ['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg'];
+
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()))
+);
+
+self.addEventListener('activate', e =>
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  )
+);
+
+self.addEventListener('fetch', e => {
+  if (new URL(e.request.url).origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
